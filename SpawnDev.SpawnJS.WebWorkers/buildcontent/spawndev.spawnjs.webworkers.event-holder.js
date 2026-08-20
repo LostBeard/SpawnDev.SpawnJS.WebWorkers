e908854518677f467eb2f4ee49bdbcf645000bd2
Todd Tanner <todd@spawndev.com>
2026-08-20 18:08:04 -0400

    v2.1.8 - Pickup changes to JS that did not get picked up last release.

--- SpawnDev.SpawnJS.WebWorkers/buildcontent/spawndev.spawnjs.webworkers.event-holder.js
+++ SpawnDev.SpawnJS.WebWorkers/buildcontent/spawndev.spawnjs.webworkers.event-holder.js
@@ -23,56 +23,59 @@
             _missedConnections.push(e.ports[0]);
         };
     } else if (globalThisTypeName == 'ServiceWorkerGlobalScope') {
-        // Starting Blazor requires using importScripts inside async functions
-        // e.waitUntil is used during the install event to allow importScripts inside async functions
-        // it is resolved after loading is complete
-        let holdEvents = true;
-        let missedServiceWorkerEvents = [];
-        function handleMissedEvent(e) {
-            if (!holdEvents) return;
-            consoleLog('ServiceWorker missed event:', e.type, e);
-            if (e.respondWith) {
-                // fetch and canmakepayment ExtendableEvents use respondWith
-                var responsePromise = new Promise(function (resolve, reject) {
-                    e.responseResolve = resolve;
-                    e.responseReject = reject;
-                });
-                e.respondWith(responsePromise);
-            } else if (e.waitUntil) {
-                // all other ExtendableEvents use waitUntil
-                var waitUntilPromise = new Promise(function (resolve, reject) {
-                    e.waitResolve = resolve;
-                    e.waitReject = reject;
-                });
-                e.waitUntil(waitUntilPromise);
+        var isExtensionScope = globalThis.location?.href && globalThis.location.href.indexOf('-extension://') !== -1;
+        if (!isExtensionScope) {
+            // .Net Wasm startup is async. This holds the synchronously fired ewvents for so they are available when .Net Wasm starts
+            // !isBackgroundExtensionScript is used to prevent this from attaching events when i na browser extension service worker because it will
+            // cause script suspend/resume to fail withotu ANY error.
+            let holdEvents = true;
+            let missedServiceWorkerEvents = [];
+            function handleMissedEvent(e) {
+                if (!holdEvents) return;
+                consoleLog('ServiceWorker missed event:', e.type, e);
+                if (e.respondWith) {
+                    // fetch and canmakepayment ExtendableEvents use respondWith
+                    var responsePromise = new Promise(function (resolve, reject) {
+                        e.responseResolve = resolve;
+                        e.responseReject = reject;
+                    });
+                    e.respondWith(responsePromise);
+                } else if (e.waitUntil) {
+                    // all other ExtendableEvents use waitUntil
+                    var waitUntilPromise = new Promise(function (resolve, reject) {
+                        e.waitResolve = resolve;
+                        e.waitReject = reject;
+                    });
+                    e.waitUntil(waitUntilPromise);
+                }
+                missedServiceWorkerEvents.push(e);
             }
-            missedServiceWorkerEvents.push(e);
+            globalThis.addEventListener('activate', handleMissedEvent);
+            globalThis.addEventListener('backgroundfetchabort', handleMissedEvent);
+            globalThis.addEventListener('backgroundfetchclick', handleMissedEvent);
+            globalThis.addEventListener('backgroundfetchfail', handleMissedEvent);
+            globalThis.addEventListener('backgroundfetchsuccess', handleMissedEvent);
+            globalThis.addEventListener('canmakepayment', handleMissedEvent);
+            globalThis.addEventListener('contentdelete', handleMissedEvent);
+            globalThis.addEventListener('cookiechange', handleMissedEvent);
+            globalThis.addEventListener('fetch', handleMissedEvent);
+            globalThis.addEventListener('install', handleMissedEvent);
+            globalThis.addEventListener('message', handleMissedEvent);
+            globalThis.addEventListener('messageerror', handleMissedEvent);
+            globalThis.addEventListener('notificationclick', handleMissedEvent);
+            globalThis.addEventListener('notificationclose', handleMissedEvent);
+            globalThis.addEventListener('paymentrequest', handleMissedEvent);
+            globalThis.addEventListener('periodicsync', handleMissedEvent);
+            globalThis.addEventListener('push', handleMissedEvent);
+            globalThis.addEventListener('pushsubscriptionchange', handleMissedEvent);
+            globalThis.addEventListener('sync', handleMissedEvent);
+            // This method will be called by Blazor WASM when it starts up to collect missed events and handle them
+            globalThis.GetMissedServiceWorkerEvents = function () {
+                holdEvents = false;
+                var ret = missedServiceWorkerEvents;
+                missedServiceWorkerEvents = [];
+                return ret;
+            };
         }
-        globalThis.addEventListener('activate', handleMissedEvent);
-        globalThis.addEventListener('backgroundfetchabort', handleMissedEvent);
-        globalThis.addEventListener('backgroundfetchclick', handleMissedEvent);
-        globalThis.addEventListener('backgroundfetchfail', handleMissedEvent);
-        globalThis.addEventListener('backgroundfetchsuccess', handleMissedEvent);
-        globalThis.addEventListener('canmakepayment', handleMissedEvent);
-        globalThis.addEventListener('contentdelete', handleMissedEvent);
-        globalThis.addEventListener('cookiechange', handleMissedEvent);
-        globalThis.addEventListener('fetch', handleMissedEvent);
-        globalThis.addEventListener('install', handleMissedEvent);
-        globalThis.addEventListener('message', handleMissedEvent);
-        globalThis.addEventListener('messageerror', handleMissedEvent);
-        globalThis.addEventListener('notificationclick', handleMissedEvent);
-        globalThis.addEventListener('notificationclose', handleMissedEvent);
-        globalThis.addEventListener('paymentrequest', handleMissedEvent);
-        globalThis.addEventListener('periodicsync', handleMissedEvent);
-        globalThis.addEventListener('push', handleMissedEvent);
-        globalThis.addEventListener('pushsubscriptionchange', handleMissedEvent);
-        globalThis.addEventListener('sync', handleMissedEvent);
-        // This method will be called by Blazor WASM when it starts up to collect missed events and handle them
-        globalThis.GetMissedServiceWorkerEvents = function () {
-            holdEvents = false;
-            var ret = missedServiceWorkerEvents;
-            missedServiceWorkerEvents = [];
-            return ret;
-        };
     }
 })()
